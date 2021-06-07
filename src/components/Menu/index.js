d208aac19ee6ca1f405197f5b7f12579cf3c9a21
Ramon Cibas <ramon.cibas@hotmail.com>
2021-06-07 19:12:29 -0300

    Menu component

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,18 +5,10 @@ import { useNavigation } from "@react-navigation/native";
 
 export default function Menu() {
   const navigation = useNavigation();
-  
+
   return (
-    <ButtonMenu
-      onPress={() => {
-        navigation.openDrawer();
-      }}
-    >
-      <Feather
-        name="menu"
-        size={40}
-        color="#fff"
-      />
+    <ButtonMenu onPress={() => { navigation.openDrawer() }}>
+      <Feather name="menu" size={40} color="#fff" />
     </ButtonMenu>
   );
 }
