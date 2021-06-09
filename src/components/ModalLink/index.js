e9300feda3525c199c8e42f99e48af99c81f85e2
Ramon Cibas <ramon.cibas@hotmail.com>
2021-06-09 19:14:17 -0300

    passing data to modal

--- src/components/ModalLink/index.js
+++ src/components/ModalLink/index.js
@@ -20,19 +20,19 @@ import {
   ShortLinkUrl,
 } from "./styles";
 
-export default function ModalLink({ onClose }) {
+export default function ModalLink({ onClose, data }) {
 
   // Function to Copy the Link
   function copyLink() {
-    Clipboard.setString("https://google.com");
-    alert("Link copied successfully!");
+    Clipboard.setString(data.link);
+    alert("Link copiado com sucesso!");
   }
 
   // Function to Share the Link
   async function handleShare() {
     try {
       const result = await Share.share({
-        message: `Link: https://google.com`,
+        message: `Link: ${data.link}`,
       });
 
       if (result.action === Share.sharedAction) {
@@ -69,11 +69,11 @@ export default function ModalLink({ onClose }) {
 
         <LinkArea>
           <Title>Link encurtado</Title>
-          <LongUrl numberOfLines={1}>https://google.com</LongUrl>
+          <LongUrl numberOfLines={1}>{data.long_url}</LongUrl>
 
           <ShortLinkArea activeOpacity={1} onPress={ copyLink }>
             <ShortLinkUrl numberOfLines={1}>
-              https://bit.ly/ds123asds
+              {data.link}
             </ShortLinkUrl>
             <TouchableOpacity onPress={ copyLink }>
               <Feather name="copy" color="#fff" size={25} />
